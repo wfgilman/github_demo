3625fbd823a114a6d6061df064e65c020f0d4f57
Sean McRoskey <_@_>
2017-04-05 19:23:56 -0700

    Milestone 1, add a description field for the repo

--- GithubDemo/GithubRepo.swift
+++ GithubDemo/GithubRepo.swift
@@ -21,6 +21,7 @@ class GithubRepo: CustomStringConvertible {
     var ownerAvatarURL: String?
     var stars: Int?
     var forks: Int?
+    var repoDescription: String?
     
     // Initializes a GitHubRepo from a JSON dictionary
     init(jsonResult: NSDictionary) {
@@ -43,6 +44,10 @@ class GithubRepo: CustomStringConvertible {
             if let ownerAvatarURL = owner["avatar_url"] as? String {
                 self.ownerAvatarURL = ownerAvatarURL
             }
+        }
+        
+        if let repoDescription = jsonResult["description"] as? String {
+            self.repoDescription = repoDescription
         }
     }
     
@@ -98,6 +103,7 @@ class GithubRepo: CustomStringConvertible {
             "\n\t[Stars: \(self.stars!)]" +
             "\n\t[Forks: \(self.forks!)]" +
             "\n\t[Owner: \(self.ownerHandle!)]" +
-            "\n\t[Avatar: \(self.ownerAvatarURL!)]"
+            "\n\t[Avatar: \(self.ownerAvatarURL!)]" +
+            "\n\t[Description: \(self.repoDescription!)"
     }
 }
